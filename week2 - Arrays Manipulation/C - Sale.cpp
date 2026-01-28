c885eef0f59b05d1ce17b6eda61cd52e53b133a2
Abdelrahman Ahmed Yasser <92957347+0Abdelrahman1@users.noreply.github.com>
2026-01-28 19:00:50 +0200

    Add Sale.cpp for array manipulation problem solution

---
+++ week2 - Arrays Manipulation/C - Sale.cpp
@@ -0,0 +1,48 @@
+// https://codeforces.com/group/iQQI5TD7io/contest/665354/problem/C
+
+//#pragma GCC optimize ("O3", "no-stack-protector", "omit-frame-pointer", "tree-parallelize-loops=4", "profile-generate", "profile-use", "prefetch-loop-arrays", "ipa-cp", "ipa-sra", "ivopts", "loop-interchange")
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef LOCAL // in cmake add_definitions(-DLOCAL)
+#include "Debug.cpp"
+#define FileI(fileName)
+#define FileO(fileName)
+#define FastIO
+#else
+#define FastIO ios_base::sync_with_stdio(0); cin.tie(0); cerr.tie(0); cout.tie(0);
+#define FileI(fileName) freopen(fileName, "r", stdin)
+#define FileO(fileName) freopen(fileName, "w", stdout)
+#define debug(...) void()
+#define __print(...) void()
+#define Time(i, x...) x
+#endif
+#define un unsigned
+#define ld long double
+#define LL long long
+#define int LL
+#define allc(x) begin(x), end(x)
+#define rallc(x) rbegin(x), rend(x)
+#define all(x,i,j) begin(x) + i, begin(x) + j
+#define Ceil(x, y) (LL)((((x) ^ (y)) > 0) ? ((x) + (y) + array{-1, 1}[x < 0]) / (y) : (x) / (y))
+#define endl(IF) cout << (IF ? "\n" : "")
+#define Test int TC, _(0); cin >> TC; while(_++ < TC && endl(_ - 1))
+
+void Solve()
+{
+    int n, m; cin >> n >> m;
+    vector<LL> a(n); for (auto& x : a) cin >> x;
+    sort(allc(a));
+    a.resize(m);
+    while (a.size() && a.back() > 0) a.pop_back();
+    cout << -accumulate(allc(a), 0ll);
+
+}
+
+signed main()
+{
+    FastIO
+    Solve();
+
+    return 0;
+}
