2f70534efe1d44e207043586b7cb25b121b44fac
Abdelrahman Ahmed Yasser <92957347+0Abdelrahman1@users.noreply.github.com>
2026-01-28 19:00:13 +0200

    Create B - Xenia and Ringroad.cpp

---
+++ week2 - Arrays Manipulation/B - Xenia and Ringroad.cpp
@@ -0,0 +1,46 @@
+//https://codeforces.com/group/iQQI5TD7io/contest/665354/problem/B
+
+//#pragma GCC optimize ("O3", "no-stack-protector", "omit-frame-pointer", "tree-parallelize-loops=4", "profile-generate", "profile-use", "prefetch-loop-arrays", "ipa-cp", "ipa-sra", "ivopts", "loop-interchange")
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef LOCAL // in cmake add_definitions(-DLOCAL)
+#include "Debug.cpp"
+#define FileI(fileName)
+#define FileO(fileName)
+#define FastIO
+#else
+#define FastIO ios_base::sync_with_stdio(0); cin.tie(0); cerr.tie(0); cout.tie(0);
+#define FileI(fileName) freopen(fileName, "r", stdin)
+#define FileO(fileName) freopen(fileName, "w", stdout)
+#define debug(...) void()
+#define __print(...) void()
+#define Time(i, x...) x
+#endif
+#define un unsigned
+#define ld long double
+#define LL long long
+#define int LL
+#define allc(x) begin(x), end(x)
+#define rallc(x) rbegin(x), rend(x)
+#define all(x,i,j) begin(x) + i, begin(x) + j
+#define Ceil(x, y) (LL)((((x) ^ (y)) > 0) ? ((x) + (y) + array{-1, 1}[x < 0]) / (y) : (x) / (y))
+#define endl(IF) cout << (IF ? "\n" : "")
+#define Test int TC, _(0); cin >> TC; while(_++ < TC && endl(_ - 1))
+
+void Solve()
+{
+    int cur = 0, ans = 0;
+    int n, m; cin >> n >> m;
+    for (int i = 0, x; i < m; i++)
+        cin >> x, ans += (--x - cur + n) % n, cur = x;
+    cout << ans;
+}
+
+signed main()
+{
+    FastIO
+    Solve();
+
+    return 0;
+}
