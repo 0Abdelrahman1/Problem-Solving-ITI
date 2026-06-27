15f1028bc1a55ef30f4597ce0cfde077ce94a9d8
Abdelrahman_ElWarraky <bodyelwarraky@gmail.com>
2026-06-27 15:43:02 +0300

    Adding Interview Preparing Problems

---
+++ Interview Preparing/Solution/Solution/remove-nth-node-from-end-of-list.cs
@@ -0,0 +1,32 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+public class removenthnodefromendoflist {
+    public ListNode RemoveNthFromEnd(ListNode head, int n)
+    {
+        int len = 0;
+        ListNode cur = head;
+
+        while (cur != null)
+        {
+            len++;
+            cur = cur.next;
+        }
+        if (len == n) return head.next;
+
+        cur = head;
+        for (int i = 0; i < len - n - 1; i++)
+            cur = cur.next;
+
+        cur.next = cur.next.next;
+        return head;
+    }
+}
